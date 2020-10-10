870933d4c46026531b7a6eb8fe91faf6402ba1b0
kartiknawal <kartiknawal.7@gmail.com>
2020-10-10 07:57:27 +0530

    [Kartik] Add. start game

--- Program.cs
+++ Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 
-namespace TicTacToeGame
+namespace TikTacToeGame
 {
-    class Program
+    public class TikTacToeGame
     {
         static void Main(string[] args)
         {
-            
+            char[] board = startGame();
+
+        }
+        public static char[] startGame()
+        {
+            char[] board = new char[10];
+            for (int i = 0; i < 10; i++)
+            {
+                board[i] = ' ';
+            }
+            return board;
         }
     }
 }
